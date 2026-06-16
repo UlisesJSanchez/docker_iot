45744dfcd1ef3819aead46ad4c56fe0516f2afea
Ulises Sánchez <ulisan16@gmail.com>
2026-06-16 14:59:01 -0300

    flask 1 ejercicio

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -10,3 +10,35 @@ if(btnDelete) {
     });
   })
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("theme-toggle");
+    const themeStyle = document.getElementById("theme-style");
+    
+    const lightThemeUrl = "https://bootswatch.com/5/cosmo/bootstrap.min.css";
+    const darkThemeUrl = "https://bootswatch.com/5/darkly/bootstrap.min.css";
+
+    const savedTheme = localStorage.getItem("theme") || "light";
+
+    const applyTheme = (theme) => {
+        if (theme === "dark") {
+            themeStyle.setAttribute("href", darkThemeUrl);
+            themeToggle.className = "btn btn-light me-4";
+            themeToggle.textContent = "Light";
+        } else {
+            themeStyle.setAttribute("href", lightThemeUrl);
+            themeToggle.className = "btn btn-dark me-4";
+            themeToggle.textContent = "Dark";
+        }
+    };
+
+    applyTheme(savedTheme);
+
+    themeToggle.addEventListener("click", () => {
+        const currentTheme = themeStyle.getAttribute("href") === lightThemeUrl ? "light" : "dark";
+        const newTheme = currentTheme === "light" ? "dark" : "light";
+        
+        applyTheme(newTheme);
+        localStorage.setItem("theme", newTheme);
+    });
+});
--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -1,12 +1,15 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 import logging, os, asyncio, aiomysql, traceback, locale
+import ssl
+from aiomqtt import Client, ProtocolVersion
 import matplotlib.pyplot as plt
 from io import BytesIO
 
 token=os.environ["TB_TOKEN"]
 
 logging.basicConfig(format='%(asctime)s - TelegramBot - %(levelname)s - %(message)s', level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info("se conectó: " + str(update.message.from_user.id))
@@ -18,22 +21,100 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         apellido=update.message.from_user.last_name
     else:
         apellido=""
-    kb = [["temperatura"],["humedad"],["gráfico temperatura"],["gráfico humedad"]]
-    await context.bot.send_message(update.message.chat.id, text="Bienvenido al Bot "+ nombre + " " + apellido,reply_markup=ReplyKeyboardMarkup(kb))
+    #kb = [["setpoint"],["periodo"],["destello"],["modo"]]
+    await context.bot.send_message(update.message.chat.id, text="¡Bienvenido al Bot "+ nombre + " " + apellido + "!") # reply_markup=ReplyKeyboardMarkup(kb)
 
-async def acercade(update: Update, context):
-    await context.bot.send_message(update.message.chat.id, text="Este bot fue creado para el curso de IoT FIO")
+async def setpoint(update: Update, context):
+    logging.info("Setpoint: " + str(context.args))
+    if context.args:
+        setpoint = context.args[0]
+        try:
+            float(setpoint)
+            await publicar_mqtt(f'{os.environ["MAC_ID"]}/setpoint', setpoint)
+            await context.bot.send_message(update.message.chat.id, text=f"Setpoint actualizado a {setpoint} °C.")
+        except ValueError:
+            await context.bot.send_message(update.message.chat.id, text="Valor de setpoint no válido. Se debe ingresar un número, por ejemplo: /setpoint 25.")
+    else:
+        await context.bot.send_message(update.message.chat.id, text="Se debe ingresar un valor de setpoint, por ejemplo: /setpoint 25.")
+
+async def periodo(update: Update, context):
+    logging.info("Periodo: " + str(context.args))
+    if context.args:
+        periodo = context.args[0]
+        try:
+            int(periodo)
+            await publicar_mqtt(f'{os.environ["MAC_ID"]}/periodo', periodo)
+            await context.bot.send_message(update.message.chat.id, text=f"Periodo actualizado a {periodo} segundos.")
+        except ValueError:
+            await context.bot.send_message(update.message.chat.id, text="Valor de periodo no válido. Se debe ingresar un número entero, por ejemplo: /periodo 10.")
+    else:
+        await context.bot.send_message(update.message.chat.id, text="Se debe ingresar un valor de periodo, por ejemplo: /periodo 10.")
+
+async def destello(update: Update, context):
+    logging.info("Destello: " + str(context.args))
+    if context.args:
+        await context.bot.send_message(update.message.chat.id, text="El comando destello no requiere argumentos. Para activarlo simplemente ingrese: /destello.")
+    else:
+        await publicar_mqtt(f'{os.environ["MAC_ID"]}/destello', 'destello')
+        await context.bot.send_message(update.message.chat.id, text="Destello activado.")
+
+async def modo(update: Update, context):
+    logging.info("Modo: " + str(context.args))
+    if context.args:
+        modo = context.args[0].lower()
+        if modo in ['auto', 'manual']:
+            await publicar_mqtt(f'{os.environ["MAC_ID"]}/modo', modo)
+            await context.bot.send_message(update.message.chat.id, text=f"Modo actualizado a {modo}.")
+        else:
+            await context.bot.send_message(update.message.chat.id, text="Modo no válido. Únicamente se aceptan 'auto' o 'manual', por ejemplo: /modo auto.")
+    else:
+        await context.bot.send_message(update.message.chat.id, text="Se debe ingresar el modo de operación, por ejemplo: /modo auto.")
+
+async def rele(update: Update, context):
+    logging.info("Réle: " + str(context.args))
+    if context.args:
+        estado = context.args[0].lower()
+        if estado in ['on', 'off']:
+            await publicar_mqtt(f'{os.environ["MAC_ID"]}/rele', estado)
+            await context.bot.send_message(update.message.chat.id, text=f"Estado del relé actualizado a {estado}.")
+        else:
+            await context.bot.send_message(update.message.chat.id, text="Estado no válido. Únicamente se aceptan 'on' o 'off', por ejemplo: /rele on.")
+    else:
+        await context.bot.send_message(update.message.chat.id, text="Se debe ingresar el estado del relé, por ejemplo: /rele on.")
+
+async def publicar_mqtt(topico: str, mensaje: str):
+    
+    tls_context = ssl.create_default_context()
+    tls_context.check_hostname = False
+    tls_context.verify_mode = ssl.CERT_NONE
+
+    try:
+        async with Client(
+            hostname=os.environ["SERVIDOR"],
+            username=os.environ.get("MQTT_USR"),
+            password=os.environ.get("MQTT_PASS"),
+            protocol=ProtocolVersion.V311,
+            port=int(os.environ["PUERTO_MQTTS"]),
+            tls_context=tls_context,
+        ) as client:
+            await client.publish(topico, mensaje, qos=1, retain=True)
+            logging.info(f"Mensaje MQTT publicado -> Tópico: {topico} | Mensaje: {mensaje}")
+    except Exception as e:
+        logging.error(f"Error al publicar en MQTT: {e}")
 
 async def kill(update: Update, context):
     logging.info(context.args)
-    if context.args and context.args[0] == '@e':
-        await context.bot.send_animation(update.message.chat.id, "CgACAgEAAxkBAAOPZkuctzsWZVlDSNoP9PavSZmH5poAAmUCAALrx0lEVKaX7K-68Ns1BA")
-        await asyncio.sleep(6)
-        await context.bot.send_message(update.message.chat.id, text="¡¡¡Ahora estan todos muertos!!!")
-    else:
-        await context.bot.send_message(update.message.chat.id, text="☠️ ¡¡¡Esto es muy peligroso!!! ☠️")
+    await context.bot.send_message(update.message.chat.id, text="Borrando base de datos en...")
+    await asyncio.sleep(1)
+    await context.bot.send_message(update.message.chat.id, text="3...")
+    await asyncio.sleep(1)
+    await context.bot.send_message(update.message.chat.id, text="2...")
+    await asyncio.sleep(1)
+    await context.bot.send_message(update.message.chat.id, text="1...")
+    await asyncio.sleep(1)
+    await context.bot.send_animation(update.message.chat.id, "BQACAgEAAxkBAAMIahiDRUBTDkaQwW0IzR9oBa5vwA8AApwFAALsoslEi568YZjqkuc7BA")
         
-async def medicion(update: Update, context):
+"""async def medicion(update: Update, context):
     logging.info(update.message.text)
     sql = f"SELECT timestamp, {update.message.text} FROM mediciones ORDER BY timestamp DESC LIMIT 1"
     conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
@@ -51,11 +132,20 @@ async def medicion(update: Update, context):
                                     text="La última {} es de {} {},\nregistrada a las {:%H:%M:%S %d/%m/%Y}"
                                     .format(update.message.text, str(r[1]).replace('.',','), unidad, r[0]))
         logging.info("La última {} es de {} {}, medida a las {:%H:%M:%S %d/%m/%Y}".format(update.message.text, r[1], unidad, r[0]))
-    conn.close()
+    conn.close()"""
 
-async def graficos(update: Update, context):
+"""async def graficos(update: Update, context):
     logging.info(update.message.text)
-    sql = f"SELECT timestamp, {update.message.text.split()[1]} FROM mediciones where id mod 2 = 0 AND timestamp >= NOW() - INTERVAL 1 DAY ORDER BY timestamp"
+    sql = fSELECT timestamp, {update.message.text.split()[1]}
+            FROM (
+                SELECT timestamp, {update.message.text.split()[1]},
+                    ROW_NUMBER() OVER (ORDER BY id) AS rn
+                FROM mediciones
+                WHERE timestamp >= NOW() - INTERVAL 1 DAY
+                AND sensor_id LIKE 'sensor_1'
+            ) AS t
+            WHERE rn % 2 = 0
+            ORDER BY timestamp
     conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
                                     user=os.environ["MARIADB_USER"],
                                     password=os.environ["MARIADB_USER_PASS"],
@@ -75,17 +165,21 @@ async def graficos(update: Update, context):
         buffer = BytesIO()
         fig.tight_layout()
         fig.savefig(buffer, format='png')
+        plt.close()
         buffer.seek(0)
         await context.bot.send_photo(chat_id=update.effective_chat.id, photo=buffer)
-    conn.close()
+        buffer.close()
+    conn.close()"""
 
 def main():
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('acercade', acercade))
+    application.add_handler(CommandHandler('setpoint', setpoint))
     application.add_handler(CommandHandler('kill', kill))
-    application.add_handler(MessageHandler(filters.Regex("^(temperatura|humedad)$"), medicion))
-    application.add_handler(MessageHandler(filters.Regex("^(gráfico temperatura|gráfico humedad)$"), graficos))
+    application.add_handler(CommandHandler('periodo', periodo))
+    application.add_handler(CommandHandler('destello', destello))
+    application.add_handler(CommandHandler('modo', modo))
+    application.add_handler(CommandHandler('rele', rele))
     application.run_polling()
 
 if __name__ == '__main__':
